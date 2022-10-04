93aaa29307e4c1ba0f64b411eed2c766f45cc6c2
Ezra Mirek <113930317+emirek2@users.noreply.github.com>
2022-10-04 17:25:47 +0000

    for loops

--- main.py
+++ main.py
@@ -9,9 +9,23 @@
 #importing modules
 #work on creating and understanding functions
 
-from decision_making import decisions
-from comparison_operators import comparison
+# from decision_making import decisions
+# from comparison_operators import comparison
 # from dictionaries_loops import dictionaries_loops
-
-decisions()
-comparison()
+from loops import loop
+# from enumerators import enumerator
+# from list_comprehension import list_comprehensions
+# from logical_operators import logic_operator
+# from min_max import minmax
+# from random import randoms
+# from ranges import range
+# from while_loops
+# decisions()
+# comparison()
+loop()
+# enumerator()
+# list_comprehensions()
+# logic_operator()
+# minmax()
+# randoms()
+# range()
